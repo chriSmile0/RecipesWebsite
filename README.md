8bc14262c5cdd49fe0d6802f4c74ee51f8bb2a99
chriSmile0 <crspitz@outlook.fr>
2024-02-13 17:02:24 +0100

    step1

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 ### Presentation
 (mivindo) This is a project in collaboration with @chriSmile0. I wanted to build a new recipe website and asked for his help. First, I decided what type of content I wanted to create. 
 I wanted to write recipes, with images and an interactive interface. Whether for 2, 4 or 6 people, I want the readers to be able to change the quantities or even choose the additional ingredients in real time.
+
+
+### Render 
+![IMG1](screens/H_A1.png)
+![IMG1](screens/A2_A3.png)
+![IMG1](screens/A3.png)
+![IMG1](screens/A4.png)
+![IMG1](screens/footer.png)
---
+++ index.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="chriSmile0">
+    <meta name="description" content="Mimi Recipes">
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" 
+          rel="stylesheet">
+	<link rel="stylesheet" 
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <title>Les Recettes de Mimi</title>
+</head>
+
+<body>
+	<header id="H">
+        <h2>Les Recettes de Mimi</h2>
+        <nav>
+			<a href="#A1">Favoris</a>
+			<a href="#A2">Découvertes</a>
+			<a href="#A3">Mes Ingrédients</a>
+			<a href="#A4">Ta Recette</a>
+        </nav>
+	</header>
+
+	<section>
+        <h2>Favoris</h2>
+        <article id="A1">
+            <div class="container">
+				<aside>
+					<h4>Le poulet Yassa</h4>
+					<img src="imgs/choucroute.jpg">
+					<p>
+						Ce plat sénégalais regorge de richesse et d'arômes
+						car on y retrouvent du citron, de la moutarde, des oignons et des olives
+						des ingrédients que l'on peut retrouver au sud de la France 
+						afin d'avoir un poulet yassa très frais et local.
+					</p>
+            	</aside>
+			</div>
+        </article>
+        <h2>Découvertes</h2>
+        <article id="A2">
+			<div class="container">
+				<aside>
+					<h4>La Choucroute</h4>
+					<img src="imgs/choucroute.jpg">
+					<p>
+						Un plat typique de la région qui m'a acceuillit pendant
+						études qui peut se déguster en toute saison car il se décline
+						en plusieurs version.
+					</p>	
+            	</aside>
+			</div>
+        </article>
+		<h2>Mes Ingrédients</h2>
+		<article id="A3">
+            <div class="container">
+                <aside>
+                    <h4>Manioc</h4>
+                    <img src="imgs/manioc.jpg">
+					<p>
+						...
+                    </p>
+                </aside>
+                <aside>
+                    <h4>Banane Plantain</h4>
+					<img src="imgs/banane_plantain.jpg">
+					<p>
+						...
+                    </p>
+                </aside>
+                <aside>
+                    <h4>Patate Douce</h4>
+					<img src="imgs/patate_douce.jpg">
+                    <p>
+						...
+                    </p>
+                </aside>
+            </div>
+		<article id="A3">
+    </section>
+
+	<h2>Ta Recette</h2>
+    <form name="contacts" id="A4" method="POST" onsubmit="return verifform(this)">
+        <fieldset>
+			<div id="Nom">
+                <label for="nom">Nom de la Recette</label>
+                <input id="nom" type="text" placeholder="Nom" aria-labelledby="nom">
+            </div>
+			<div>
+				<label for="type_prepa">Type de Préparation</label>
+				<select name="type_prepa" aria-labelledby="type_prepa">
+					<option value="sucree">Sucrée</option>
+					<option value="salee">Salée</option>
+					<option value="mixte">Mixte</option>
+				</select>
+			</div>
+			<div>
+                <label for="ingredients">Ingrédients</label>
+                <textarea 	name="message"  aria-labelledby="message" 
+							placeholder="Ingrédient1;Ingrédient2">
+				</textarea>
+            </div>
+			<div>
+                <label for="prepa">Préparation</label>
+                <textarea 	name="prepa"  aria-labelledby="prepa" 
+							placeholder="Étape1;Étape2">
+				</textarea>
+            </div>
+			<div>
+                <label for="description">Description</label>
+                <textarea 	name="description"  aria-labelledby="description" 
+							placeholder="Description de la recette">
+				</textarea>
+            </div>
+			<button name="submitrecette"
+              type="submit" value="EnvoyerRecette">Enregistrer</button>
+		</fieldset>
+	</form>
+
+	<div id="top">
+        <a id="top_a" href="#H">
+            <i id="top_a_i" class="material-icons">expand_less</i>
+        </a>
+    </div>
+
+	<footer>
+        <h3 style="padding-bottom:0px;margin-bottom:0px;">Mes Reseaux Sociaux</h3>
+        <br>
+        <a href="http://www.facebook.com" target="_blank" title="Facebook">
+        <!--_blank sert a ouvrir la page facebook sur un autre onglet-->
+            <i class="fa fa-facebook-square" style="font-size:40px;"></i>
+        </a>
+        <a href="http://www.twitter.com" target="_blank" title="Twitter">
+        <!--_blank sert a ouvrir la page twitter sur un autre onglet-->
+            <i class="fa fa-twitter-square" style="font-size:40px;"></i>
+        </a>
+        <a href="http://www.instagram.com" target="_blank" title="Instagram">
+        <!--_blank sert a ouvrir la page instagram sur un autre onglet-->
+            <i class="fa fa-instagram" style="font-size:40px"></i>
+        </a>
+    </footer>
+	<script src="my_js.js"></script>
+</body>
+</html>
